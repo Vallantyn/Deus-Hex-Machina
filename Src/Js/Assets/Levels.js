48f17cd7007b4ac421807cf55893e3bd7c559ed0
LudovicLeng <ludovic.leng@gmail.com>
2013-04-19 10:47:52 +0200

    Dernier changement de level

--- Src/Js/Assets/Levels.js
+++ Src/Js/Assets/Levels.js
@@ -1,5 +1,6 @@
 ﻿var Levels =
-    [
+    [	
+		
         {
             name: "00",
             size: 4,
@@ -33,7 +34,7 @@
                 }
             ]
         },
-        {
+		{
             name: "01",
             size: 8,
             tiles:
@@ -60,7 +61,7 @@
                 type: "Recepter",
                 recepterColor: "#FFFFFF"
                 }
-            },
+           },
             shelf:
             [
                 {
@@ -77,62 +78,10 @@
                 }
             ]
         },
+        
+		
 		{
 		    name: "02",
-		    size: 8,
-		    tiles:
-            {
-
-                41:
-                    {
-                        type: "Emitter",
-                        emitterColor: "#FFFFFF",
-                        direction: 3
-                    },
-                47:
-                {
-                type: "Recepter",
-                recepterColor: "#FFFFFF"
-                },
-                52:
-                {
-                type: "Recepter",
-                recepterColor: "#0000FF"
-                },
-                124:
-                {
-                type: "Recepter",
-                recepterColor: "#FFFFFF"
-                },
-                124:
-                {
-                type: "Recepter",
-                recepterColor: "#FFFFFF"
-                }
-            },
-		    shelf:
-            [
-                {
-                    type: "Block",
-                    count: 99
-                },
-                {
-                    type: "Empty",
-                    count: 99
-                },
-                {
-                    type: "Mirror",
-                    count: 4
-                },
-				{
-				    type: "Filter",
-				    count: 1,
-				    filterColor: "#0000FF"
-				}
-            ]
-		},
-		{
-		    name: "03",
 		    size: 8,
 		    tiles:
             {
@@ -200,7 +149,7 @@
             ]
 		},
 		{
-		    name: "04",
+		    name: "03",
 		    size: 8,
 		    tiles:
             {
@@ -261,5 +210,221 @@
                     filterColor: "#FF0000"
                 }
             ]
+		},
+		{
+		    name: "04",
+		    size: 8,
+		    tiles:
+            {
+				3:
+				{
+					type: "Recepter",
+					recepterColor :"#00FF00"
+				},
+                67:
+                {
+                    type: "Emitter",
+                    emitterColor: "#FFFFFF",
+                    direction: 5
+                },
+
+                165:
+                {
+                type: "Filter",
+                filterColor: "#0000FF"
+                },
+				
+				166:
+				{
+					type: "Recepter",
+					recepterColor :"#FFFFFF"
+				},
+				
+				152:
+				{
+					type: "Recepter",
+					recepterColor :"#0000FF"
+				},
+
+                151:
+                {
+                type: "Filter",
+                filterColor: "#FF00FF"
+                },
+				70:
+				{
+					type: "Recepter",
+					recepterColor :"#FF00FF"
+				},
+                69:
+                {
+                type: "Filter",
+                filterColor: "#FF0000"
+                },
+				36:
+				{
+					type: "Recepter",
+					recepterColor :"#FF0000"
+				},
+
+                35:
+                {
+                type: "Filter",
+                filterColor: "#FFFF00"
+                },
+				15:
+				{
+					type: "Recepter",
+					recepterColor :"#FFFF00"
+				},
+				14:
+                {
+                type: "Filter",
+                filterColor: "#00FF00"
+                },
+				68:
+				{
+					type: "Block"
+				},
+				42:
+				{
+					type: "Block"
+				},
+				21:
+				{
+					type: "Block"
+				},
+				19:
+				{
+					type: "Block"
+				},
+				60:
+				{
+					type: "Block"
+				},
+				89:
+				{
+					type: "Block"
+				},
+				100:
+				{
+					type: "Block"
+				},
+				51:
+				{
+					type: "Block"
+				},
+				88:
+				{
+					type: "Block"
+				},
+				58:
+				{
+					type: "Block"
+				},
+				20:
+				{
+					type: "Block"
+				},
+				124:
+				{
+					type: "Block"
+				},
+				32:
+				{
+					type: "Block"
+				},
+				31:
+				{
+					type: "Block"
+				},
+				79:
+				{
+					type: "Block"
+				},
+				112:
+				{
+					type: "Block"
+				},
+				78:
+				{
+					type: "Block"
+				},
+				50:
+				{
+					type: "Block"
+				},
+				44:
+				{
+					type: "Block"
+				},
+				24:
+				{
+					type: "Block"
+				},
+				10:
+				{
+					type: "Block"
+				},
+				52:
+				{
+					type: "Block"
+				},
+				2:
+				{
+					type: "Block"
+				},
+				1:
+				{
+					type: "Block"
+				},
+				6:
+				{
+					type: "Block"
+				},
+				5:
+				{
+					type: "Block"
+				},
+				11:
+				{
+					type: "Block"
+				},
+				12:
+				{
+					type: "Block"
+				},
+				13:
+				{
+					type: "Block"
+				},
+				29:
+				{
+					type: "Block"
+				}
+				
+				
+
+            },
+		    shelf:
+            [
+                {
+                    type: "Block",
+                    count: 99
+                },
+                {
+                    type: "Empty",
+                    count: 99
+                },
+                {
+                    type: "Mirror",
+                    count: 6
+                }
+            ]
 		}
+		
+		
+		
+		
+
     ];
