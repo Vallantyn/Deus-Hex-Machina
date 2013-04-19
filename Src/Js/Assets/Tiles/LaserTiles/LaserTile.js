e35b0dfb3b1147e0165e0457d4f6ca96f3b0fb79
Julien Murtin <jul.murtin@gmail.com>
2013-04-19 02:14:27 +0200

    Laser direction bug corrected mirror reflection working

--- Src/Js/Assets/Tiles/LaserTiles/LaserTile.js
+++ Src/Js/Assets/Tiles/LaserTiles/LaserTile.js
@@ -2,31 +2,63 @@ function LaserTile(id, s, px, py)
 {
     var that = new Tile(id, s, px, py);
 
-    //that.drawLaser = function(laserData)
-    //{
-    //    var cx = laserData.context;
-    //    cx.strokeStyle = laserData.color;
-    //    cx.beginPath();
-    //    cx.moveTo(laserData.center.x , laserData.center.y );
-    //    cx.lineTo(laserData.center.x + (laserData.outer * Math.cos(laserData.angle)), laserData.center.y + (laserData.outer * Math.sin(laserData.angle)));
-    //    cx.closePath();
-    
-    //    cx.stroke();
-    //}
+    var selflaser = null;
+
+    that.Render = function (tileData)
+    {
+        var cx = tileData.context;
+
+        cx.fillStyle = that.color;
+
+        if (that.over) cx.fill();
+        cx.stroke();
+
+        //var str = id;//Math.floor(loopIndex/loopDepth);
+
+        //var w = cx.measureText(str).width/2;
+
+        //UI.Label(str, tileData.center.x - w, tileData.center.y, { textBaseline: "middle" });
+
+        that.drawLaser(selflaser, tileData);
+    },
+
+    that.drawLaser = function (laserData, tileData)
+    {
+        if (!selflaser) return;
+
+        var cx = tileData.context;
+
+        for (var i = 1; i < 4; i++)
+        {
+
+            cx.lineWidth = i * 4;
+            cx.globalAlpha = 1 / (i * 2);
+            cx.strokeStyle = selflaser.color;
+
+            var a = (3 - selflaser.to) * Math.PI / 3 + Math.PI / 6;
+
+            cx.beginPath();
+            cx.moveTo(tileData.center.x + tileData.inner * Math.cos(a), tileData.center.y + tileData.inner * Math.sin(a));
+            cx.lineTo(tileData.center.x + tileData.inner * Math.cos(a - Math.PI), tileData.center.y + tileData.inner * Math.sin(a - Math.PI));
+            cx.stroke();
+
+            cx.globalAlpha = 1;
+        }
+
+        selflaser = null;
+    }
 
     that.onLaser = function (laserData)
     {
+        var laser = {
+            from: that.id,
+            to: laserData.to,
+            color: laserData.color
+        };
 
-        //console.log(that.id);
+        selflaser = laser;
 
-        //console.log(that.id);
-        var laser = laserData;
-
-        laser.from = id;
-
-        that.laser = laser;
-
-        this.emitLaser(laser);
+        this.emitLaser(selflaser);
     }
 
     return that;
